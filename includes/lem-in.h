b7dba89e37b9723542ff2bcad1073496180a9d25
Mateo RODRIGUEZ <mrodrigu@e3r7p13.42.fr>
2018-06-08 20:08:55 +0200

    cosa

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 21:05:02 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/06/07 21:42:21 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/06/08 20:08:31 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@
 
 typedef struct	s_node
 {
-	char		*name;
-	int			id;
-	int			ant : 1;
-
+	char			*name;
+	int				id;
+	int				ant : 1;
+	int				start : 1;
+	int				end : 1;
+	int				n_links;
+	struct s_node	*links;
 }				t_node;
 
 #endif
