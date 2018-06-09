920437c1ada693d7a7e765abf0baed7d7a3428c2
Jaume GARCIA <jagarcia@e1r4p15.42.fr>
2018-06-09 04:59:50 +0200

    algoritmo

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 21:05:02 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/06/08 21:28:24 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/06/09 01:04:26 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 typedef struct		s_node
 {
 	char			*name;
-	int				id;
-	int				ant : 1;
+	int				id;  // no entiendo la funcion de id si tienes *name o viceversa
+	int				ant : 1; // en start pueden haber infinitas hormigas
 	int				start : 1;
 	int				end : 1;
-	int				n_links;
+//	int				n_links; // no hace falta solo necesitas que *links termine un NULL;
 	struct s_node	*links;
 }					t_node;
 
