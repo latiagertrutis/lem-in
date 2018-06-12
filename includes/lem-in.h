c4d97be9687c4270bd3b48dcc228ab0815e10a1d
Jaume <carterillo@hotmail.com>
2018-06-12 09:21:33 +0200

    comienzo
    compienzo

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 21:05:02 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/06/10 03:17:47 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/06/12 04:14:51 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,21 @@
 typedef struct		s_node
 {
 	char			*name;
+	int				id;
 	int				ants;
 	int				start : 1;
 	int				end : 1;
+	int				ihbt : 1;
 	int				n_links;
 	struct s_node	*links;
 }					t_node;
+
+typedef struct		s_path
+{
+	t_node			*node;
+	t_path			*next;
+	t_path			*prev;
+}
 
 typedef struct		s_data
 {
